9582d6b3667138faa1d976772ec73018e595c4ba
Nishit Detroja <nishitdetroja2260@gmail.com>
2025-07-28 15:26:08 +0530

    Fix : Android Build Fixes
    - Remove Unnecessary library directive which cause android build problem

--- Match_Card/Assets/Scripts/Managers/Gameplay Scripts/UIManager.cs
+++ Match_Card/Assets/Scripts/Managers/Gameplay Scripts/UIManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using Nishit.Class;
 using TMPro;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
--- Match_Card/Assets/Scripts/Managers/Main Menu Scripts/MainMenuManager.cs
+++ Match_Card/Assets/Scripts/Managers/Main Menu Scripts/MainMenuManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Nishit.Class;
 using TMPro;
 using UnityEngine;
